95a0d19e9dd207eb1c3f9b6bdb6966a0d9e47c2b
Thanabordee Bunditsakul <thanabordee.bu@ku.th>
2024-05-06 20:57:02 +0700

    Now in banana screen have checkbar to choose which attribute to plotgraph

--- banana_graph.py
+++ banana_graph.py
@@ -2,12 +2,17 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from check_bar import Checkbar
 class Banana_graph_maker:
     def __init__(self):
-        self.df = pd.read_csv('banana_quality.csv')
+        self.df = pd.read_csv('C:/worktoomuch/compro2 project/Banana_Quality_project/banana_quality.csv')
         self.df.dropna(inplace=True)
         self.new_df = self.df.copy()
-    def show_hist(self,name):
+    def show_hist(self,name,**kwargs):
         self.new_df[name].hist(figsize=(10, 8))
         plt.show()
+    def assoication(self):
+        pass
+k = Banana_graph_maker()
+k.show_hist(["Size",'Sweetness'])
     
--- banana_screen.py
+++ banana_screen.py
@@ -1,12 +1,38 @@
 import tkinter as tk
 from tkinter import ttk
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 from banana_graph import Banana_graph_maker
+from check_bar import Checkbar
 class Banana_application(tk.Frame):
     def __init__(self,parent):
         super().__init__(parent)
-      
+        self.graph = Banana_graph_maker()
+        self.checkbar =  Checkbar(root, ['Size', 'Weight', 'Sweetness','Softness','Ripeness','Harvesttime','Acidity','Quality'])
+        self.checkbar.pack()
+        self.quit = tk.Button(root, text='Quit', command=root.quit).pack()
+        #self.graph_button = tk.Button(root, text='hist', command=lambda: self.graph.show_hist(self.handle_hist_button())).pack()
+        self.graph_button = tk.Button(root, text='hist', command=self.print).pack()
+        #self.init_component()
+    #def init_component(self):
         
         
+       
+    def handle_hist_button(self):
+        new_list = []
+        list_of_attribute = ['Size', 'Weight', 'Sweetness','Softness','Ripeness','HarvestTime','Acidity','Quality']
+        checked = list(self.checkbar.state())
+        
+        for value, label in zip(checked,list_of_attribute):
+            if value == 1:
+                new_list.append(label)
+        if new_list != []:
+            return (new_list)
+    def print(self):
+        self.graph.show_hist(self.handle_hist_button())
+            
 
 if __name__ == "__main__":
     root = tk.Tk()
